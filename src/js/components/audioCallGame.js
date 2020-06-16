8e5de30a6828220380e2f4ec5615e9267ca1143b
Yauheni Kozak <zhenikusss@gmail.com>
2020-06-16 23:07:27 +0300

    fix: add switch pages and import js renderGamePage

--- src/js/components/audioCallGame.js
+++ src/js/components/audioCallGame.js
@@ -1,7 +1,15 @@
 import renderStartPage from './renderStartPage';
-// import renderGamePage from './renderGamePage';
+import renderGamePage from './renderGamePage';
+
+function startGame() {
+  document.querySelector('.game').remove();
+  const pageContent = document.querySelector('.page');
+  pageContent.append(renderGamePage());
+}
 
 export default function initAudioCallGame() {
   const pageContent = document.querySelector('.page');
   pageContent.append(renderStartPage());
+  document.querySelector('.game__start').addEventListener('click', startGame);
 }
+
