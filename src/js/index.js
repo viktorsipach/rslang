6bbf7b5996e76b98327dc9b5818f6654c4d6cd3d
Yauheni Kozak <zhenikusss@gmail.com>
2020-06-16 23:10:13 +0300

    fix: add call function

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
-
 import '../css/reset.css';
 import '../scss/style.scss';
+import initAudioCallGame from './components/audioCallGame';
 
-console.log('Hello')
+console.log('Hello');
+initAudioCallGame();
