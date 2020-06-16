575ef2adf677db6c4fa491c864889f694fe035b3
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-06-16 23:38:03 +0300

    feat: [Sprint] add card, sound control and counter elements to ContentBuilder

--- src/js/components/sprint/modules/ContentBuilder.js
+++ src/js/components/sprint/modules/ContentBuilder.js
@@ -1,15 +1,34 @@
+const cardMarkup = `
+  <div class="sprint__card card">
+    <div class="card__header">
+      <div class="card__header_info">
+      </div>
+      <div class="card__header_sound">
+      </div>
+    </div>
+    <div class="card__body">
+      <div class="card__body_image">
+      </div>
+      <div class="card__body_foreign-word"></div>
+      <div class="card__body_translated-word"></div>
+    </div>
+    <div class="card__control">
+      <button class="card__button card__button_false"></button>
+      <button class="card__button card__button_true"></button>
+    </div>
+  </div>
+`;
+
 class ContentBuilder {
-  constructor() {
-    this.cardMarkup = `
-
-    `;
-  }
-
   addContentToPage(parentSelector) {
     this.parentSelector = parentSelector;
     const parent = document.querySelector(this.parentSelector || '.page__sprint');
+    parent.innerHTML = '';
     ContentBuilder.addTimerToPage(parent);
     ContentBuilder.addExitButtonToPage(parent);
+    ContentBuilder.addSoundControlToPage(parent);
+    ContentBuilder.addCounterToPage(parent);
+    ContentBuilder.addCardToPage(parent);
   }
 
   static addTimerToPage(parent) {
@@ -27,8 +46,20 @@ class ContentBuilder {
   static addCardToPage(parent) {
     const card = document.createElement('div');
     card.classList.add('sprint__card', 'card');
-    card.innerHTML = this.cardMarkup;
+    card.innerHTML = cardMarkup;
     parent.append(card);
+  }
+
+  static addCounterToPage(parent) {
+    const counter = document.createElement('div');
+    counter.classList.add('sprint__counter');
+    parent.append(counter);
+  }
+
+  static addSoundControlToPage(parent) {
+    const soundControl = document.createElement('div');
+    soundControl.classList.add('sprint__sound-control');
+    parent.append(soundControl);
   }
 }
 
