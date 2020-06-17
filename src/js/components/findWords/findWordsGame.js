9565ad879c9e85ce94052a141e9dde5d69bfd1ab
allihach <lihach.ac@gmail.com>
2020-06-18 01:28:44 +0400

    feat: [Find words game] generating main page

--- src/js/components/findWords/findWordsGame.js
+++ src/js/components/findWords/findWordsGame.js
@@ -1,11 +1,14 @@
 import RenderFindWordsGame from './renderFindWordsGame';
+import ActionFindWordsGame from './actionFindWordsGame';
 
 export default class FindWordsGame {
     constructor() {
         this.renderFindWordsGame = new RenderFindWordsGame();
+        this.actionFindWordsGame = new ActionFindWordsGame();
     }
 
     initFindWordsGame() {
         this.renderFindWordsGame.renderStartPage();
+        this.actionFindWordsGame.startGame();
     }
 }
