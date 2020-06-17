1225c72252f1505357d38fd5f95a3a2506af3535
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-06-17 19:18:15 +0400

    fix: replace API folder inside js folder

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import '../css/reset.css';
 import '../scss/style.scss';
 import initAudioCallGame from './components/audioCallGame/audioCallGame';
 import initPuzzleGame from './components/puzzleGame/puzzleGame';
-import testDataAPI from './components/testAPI';
+import testDataAPI from './API/testGetRoundData';
 import menuHandler from './components/header.components';
 
 console.log('Hello');
