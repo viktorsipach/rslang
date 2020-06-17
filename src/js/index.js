d9f46e025986e3be0a4d22c5a4118904d1df1e60
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-06-17 18:17:08 +0400

    feat: add dataAPI functions and example

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 import '../css/reset.css';
 import '../scss/style.scss';
 import initPuzzleGame from './components/puzzleGame/puzzleGame';
+import testDataAPI from './components/testAPI';
 
 console.log('Hello');
 initPuzzleGame();
+
+testDataAPI();
