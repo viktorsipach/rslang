c5628159f6d48b0471943e7f32d5a36934e13d7c
viktor <vitek230290@gmail.com>
2020-06-17 22:00:46 +0300

    feat:[App main page] add navHandlerClick

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,8 @@
 import '../css/reset.css';
 import '../scss/style.scss';
-import initAudioCallGame from './components/audioCallGame/audioCallGame';
-import initPuzzleGame from './components/puzzleGame/puzzleGame';
 import testDataAPI from './API/testGetRoundData';
-import menuHandler from './components/header.components';
+import initApp from './components/app/app.components';
 
-console.log('Hello');
-initPuzzleGame();
-initAudioCallGame();
-
-menuHandler();
+initApp()
 
 testDataAPI();
