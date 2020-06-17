8f037055f8bd59a49fbf60ef5c772756042bc374
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-06-17 16:50:54 +0300

    refactor: [Sprint] refactor ContentBuilder to use single method for different content

--- src/js/components/sprint/modules/ContentBuilder.js
+++ src/js/components/sprint/modules/ContentBuilder.js
@@ -1,65 +1,53 @@
-const cardMarkup = `
-  <div class="sprint__card card">
-    <div class="card__header">
-      <div class="card__header_info">
-      </div>
-      <div class="card__header_sound">
-      </div>
-    </div>
-    <div class="card__body">
-      <div class="card__body_image">
-      </div>
-      <div class="card__body_foreign-word"></div>
-      <div class="card__body_translated-word"></div>
-    </div>
-    <div class="card__control">
-      <button class="card__button card__button_false"></button>
-      <button class="card__button card__button_true"></button>
-    </div>
-  </div>
-`;
-
 class ContentBuilder {
+  constructor() {
+    this.cardMarkup = `
+      <div class="sprint__card card">
+        <div class="card__header">
+          <div class="card__header_info">
+          </div>
+          <div class="card__header_sound">
+          </div>
+        </div>
+        <div class="card__body">
+          <div class="card__body_image">
+          </div>
+          <div class="card__body_foreign-word"></div>
+          <div class="card__body_translated-word"></div>
+        </div>
+        <div class="card__control">
+          <button class="card__button card__button_false"></button>
+          <button class="card__button card__button_true"></button>
+        </div>
+      </div>
+    `;
+  }
+
   addContentToPage(parentSelector) {
     this.parentSelector = parentSelector;
     const parent = document.querySelector(this.parentSelector || '.page__sprint');
     parent.innerHTML = '';
-    ContentBuilder.addTimerToPage(parent);
-    ContentBuilder.addExitButtonToPage(parent);
-    ContentBuilder.addSoundControlToPage(parent);
-    ContentBuilder.addCounterToPage(parent);
-    ContentBuilder.addCardToPage(parent);
+    this.addElementToPage(parent, '', 'sprint__timer');
+    this.addElementToPage(parent, '', 'sprint__exit');
+    this.addElementToPage(parent, '', 'sprint__sound-control');
+    this.addElementToPage(parent, '', 'sprint__counter');
+    this.addElementToPage(parent, this.cardMarkup, 'sprint__card', 'card');
   }
 
-  static addTimerToPage(parent) {
-    const timer = document.createElement('div');
-    timer.classList.add('sprint__timer');
-    parent.append(timer);
-  }
-
-  static addExitButtonToPage(parent) {
-    const exitButton = document.createElement('div');
-    exitButton.classList.add('sprint__exit');
-    parent.append(exitButton);
-  }
-
-  static addCardToPage(parent) {
-    const card = document.createElement('div');
-    card.classList.add('sprint__card', 'card');
-    card.innerHTML = cardMarkup;
-    parent.append(card);
-  }
-
-  static addCounterToPage(parent) {
-    const counter = document.createElement('div');
-    counter.classList.add('sprint__counter');
-    parent.append(counter);
-  }
-
-  static addSoundControlToPage(parent) {
-    const soundControl = document.createElement('div');
-    soundControl.classList.add('sprint__sound-control');
-    parent.append(soundControl);
+  addElementToPage(parent, markup, ...classes) {
+    const element = document.createElement('div');
+    const [class1, class2] = classes;
+    if (class1) {
+      if (class2) {
+        element.classList.add(class1, class2);
+      } else {
+        element.classList.add(class1);
+      }
+    }
+    if (markup) {
+      element.innerHTML = markup;
+    }
+    parent.append(element);
+    return this;
   }
 }
 
