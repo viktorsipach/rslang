a58c38d87a4b12add3ce361560cda8dd7a31c7d6
allihach <lihach.ac@gmail.com>
2020-07-03 12:48:12 +0400

    feat:[Long term statistics] add mini game stat

--- src/js/components/findWordsGame/renderFindWordsGame.js
+++ src/js/components/findWordsGame/renderFindWordsGame.js
@@ -1,4 +1,5 @@
 import {getRoundData} from '../../API/dataAPI';
+import StatisticsAPI from '../../API/statisticsAPI';
 
 class RenderFindWordsGame {
     constructor() {
@@ -187,6 +188,8 @@ class RenderFindWordsGame {
             document.querySelector('.statistics__next-button').style.display = 'none';
             document.querySelector('.statistics__header').innerHTML = 'Поздравляем!<br><p>Игра завершена!<br>Статистика Раунда';
         }
+
+        StatisticsAPI.miniGameStat('findWords', 'result');
 
         document.querySelector('.find-words').dispatchEvent(event);
     }
